6011842c50d79b033af88bde1a52b5e25d974d47
HalyangDream <847019460@qq.com>
2018-08-27 18:39:44 +0800

    完成了RecylcerView 的刷新控件

--- app/src/main/java/com/example/pullrefreshlayout/MainActivity.java
+++ app/src/main/java/com/example/pullrefreshlayout/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.pullrefreshlayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.pullrefreshlayout.activity.RecyclerActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button recyclerBtn, listViewBtn, gridViewBtn, scorllViewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerBtn = findViewById(R.id.recycler);
+        listViewBtn = findViewById(R.id.listview);
+        gridViewBtn = findViewById(R.id.gridview);
+        scorllViewBtn = findViewById(R.id.scrollview);
+
+        recyclerBtn.setOnClickListener(this);
+        listViewBtn.setOnClickListener(this);
+        gridViewBtn.setOnClickListener(this);
+        scorllViewBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.recycler:
+                Intent intent = new Intent(this, RecyclerActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.listview:
+                break;
+            case R.id.gridview:
+                break;
+            case R.id.scrollview:
+                break;
+            default:
+                break;
+        }
     }
 }
